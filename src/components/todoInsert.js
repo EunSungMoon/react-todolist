6b5a2e8bca2fa3a7486a8d60d3a3900958799f52
Eunsung Moon <eunsung9647@gmail.com>
2021-11-18 23:03:38 +0900

    리스트 추가

--- src/components/todoInsert.js
+++ src/components/todoInsert.js
@@ -1,16 +1,39 @@
-import React, { Component } from "react";
+import React from "react";
 
-class TodoInsert extends Component {
-  render() {
-    return (
+function TodoInsert() {
+
+  const [todos, setTodos] = React.useState([])
+  const [todo, setTodo] = React.useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo,
+      completed: false
+    }
+
+    setTodos([...todos].concat(newTodo))
+    setTodo("")
+  }
+
+  return (
+    <React.Fragment>
       <section>
-        <form>
-          <input placeholder="write to do"></input>
-          <button>입력</button>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="list" placeholder="write to do" onChange={e => setTodo(e.target.value)} value={todo} />
+          <button type="submit">Add Todo</button>
         </form>
       </section>
-    )
-  }
+      <section>
+        <div>
+          {todos.map((todo) => <div>{todo.text}</div>)}
+        </div>
+      </section>
+    </React.Fragment>
+  )
+
 }
 
 export default TodoInsert;
